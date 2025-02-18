90fc0cfe54c8a89242112dbc52a7eaa0943c4e90
IsabelaGMS <132318163+IsabelaGMS@users.noreply.github.com>
2025-02-18 20:42:15 +0000

    Criando a funcionalidade para cadastrar cartas

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -21,6 +21,43 @@ int main() {
         carta->densidadePopulacional = carta->populacao / carta->area;
         carta->pibPerCapita = carta->pib / carta->populacao;
     }
+    int main() {
+        
+        struct Carta carta1, carta2;
+
+        printf("Cadastro da Carta 1:\n");
+        printf("Digite o estado: ");
+        scanf("%s", carta1.estado);
+        printf("Digite o código da carta: ");
+        scanf("%s", carta1.codigo);
+        printf("Digite o nome da cidade: ");
+        scanf("%s", carta1.nomeCidade);
+        printf("Digite a população: ");
+        scanf("%d", &carta1.populacao);
+        printf("Digite a área: ");
+        scanf("%f", &carta1.area);
+        printf("Digite o PIB: ");
+        scanf("%f", &carta1.pib);
+        printf("Digite o número de pontos turísticos: ");
+        scanf("%d", &carta1.pontosTuristicos);
+        calcularIndicadores(&carta1);
+    
+        printf("\nCadastro da Carta 2:\n");
+        printf("Digite o estado: ");
+        scanf("%s", carta2.estado);
+        printf("Digite o código da carta: ");
+        scanf("%s", carta2.codigo);
+        printf("Digite o nome da cidade: ");
+        scanf("%s", carta2.nomeCidade);
+        printf("Digite a população: ");
+        scanf("%d", &carta2.populacao);
+        printf("Digite a área: ");
+        scanf("%f", &carta2.area);
+        printf("Digite o PIB: ");
+        scanf("%f", &carta2.pib);
+        printf("Digite o número de pontos turísticos: ");
+        scanf("%d", &carta2.pontosTuristicos);
+        calcularIndicadores(&carta2);
     
 
     return 0;
